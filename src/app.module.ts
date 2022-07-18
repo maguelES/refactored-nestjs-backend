0b762f096c470a2dbaf27704a37f882c1a01fdbc
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-18 11:01:56 +0800

    Port number use from config/env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
 
 @Module({
@@ -13,4 +13,6 @@ import configuration from '../config/configuration';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+}
