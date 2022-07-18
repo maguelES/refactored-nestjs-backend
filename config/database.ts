d32ffd7eb074d44c3e3e627c87143a0e8e942761
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-18 11:21:47 +0800

    Setup type orm module

---
+++ config/database.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  type: 'postgres',
+  port: 5432,
+  username: 'user',
+  password: 'password',
+  database: 'db',
+  synchronize: true,
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from '../config/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    TypeOrmModule.forRoot({ ...databaseConfig }),
   ],
   controllers: [AppController],
   providers: [AppService],
