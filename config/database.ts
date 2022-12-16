c94431d39266de9fa1c44b613999ec4c6c7c9567
Mohd Amsyar <nerokyt2@gmail.com>
2022-12-16 17:15:41 +0800

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,16 @@
-export default () => ({
+import { DataSource } from 'typeorm';
+
+export const connectionSource = new DataSource({
+  migrationsTableName: 'migrations',
   type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'db',
-  synchronize: true,
+  username: process.env.DATABASE_USER,
+  password: 'pass',
+  database: 'somehealthchecker',
+  logging: false,
+  synchronize: false,
+  entities: ['src/**/**.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscriber/**/*{.ts,.js}'],
 });
