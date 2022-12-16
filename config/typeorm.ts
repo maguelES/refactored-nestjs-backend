abb4cda498c3ed97a23d1d245eb3c079e3399829
Mohd Amsyar <nerokyt2@gmail.com>
2022-12-16 16:22:12 +0800

    Use some db values from process env

--- config/typeorm.ts
+++ config/typeorm.ts
@@ -1,11 +1,11 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 export default TypeOrmModule.forRoot({
-  host: 'localhost:5432',
+  host: process.env.DATABASE_URL ?? 'localhost:5432',
   type: 'postgres',
   port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'db',
+  username: process.env.DATABASE_USER ?? 'username',
+  password: process.env.PASSWORD ?? 'test',
+  database: process.env.DATABASE ?? 'db',
   synchronize: true,
 });
