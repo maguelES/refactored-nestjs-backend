1e721ca9de229605873a571f53eb892060bd2f38
Mohd Amsyar <nerokyt2@gmail.com>
2022-12-27 16:42:29 +0800

    Auth based config

--- config/database.config.ts
+++ config/database.config.ts
@@ -9,4 +9,9 @@ export default registerAs('database', () => ({
   name: process.env.DATABASE_SCHEMA,
   maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10) || 100,
   synchronize: false,
+  sslEnabled: process.env.DATABASE_SSL_ENABLED === 'true',
+  rejectUnauthorized: process.env.DATABASE_REJECT_UNAUTHORIZED === 'true',
+  ca: process.env.DATABASE_CA,
+  key: process.env.DATABASE_KEY,
+  cert: process.env.DATABASE_CERT,
 }));
