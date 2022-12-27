0709c77df37ebf6799478d39e0c6a7d53cff7844
Mohd Amsyar <nerokyt2@gmail.com>
2022-12-27 16:15:50 +0800

    Add missing password

--- config/database.config.ts
+++ config/database.config.ts
@@ -4,8 +4,9 @@ export default registerAs('database', () => ({
   url: process.env.DATABASE_URL ?? 'localhost',
   type: 'postgres',
   port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  username: process.env.DATABASE_USER ?? '',
+  password: process.env.DATABASE_PASSWORD ?? '',
   name: process.env.DATABASE_SCHEMA,
   maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10) || 100,
+  synchronize: false,
 }));
