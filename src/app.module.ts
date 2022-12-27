fa9582b77aaebaae5983244ee7f9cd842f60d536
Mohd Amsyar <nerokyt2@gmail.com>
2022-12-27 16:15:59 +0800

    Inject type orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
-import typeorm from '../config/typeorm/typeorm';
+import databaseConfig from '../config/database.config';
 import { MainModule } from './main/MainModule';
+import { TypeOrmService } from '../config/typeorm/type.orm.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration],
+      isGlobal: true,
+      load: [configuration, databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmService,
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
     MainModule,
-    typeorm,
   ],
   controllers: [AppController],
   providers: [AppService],
