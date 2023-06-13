765dd0b9d04fd599b442d17092ee2784e76e8779
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-13 14:05:19 +0800

    feat: add empty methods for find and save

--- src/main/controllers/test/user-pipe-test.controller.ts
+++ src/main/controllers/test/user-pipe-test.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../model/user.entity';
 import { Repository } from 'typeorm';
+import { UserRegistrationForm } from '../../data/transfers/user-registration.form';
 
 @Controller('user/test/pipe')
 export class UserPipeTestController {
@@ -11,6 +12,18 @@ export class UserPipeTestController {
   ) {}
   @Get()
   async findAll(): Promise<any> {
+    return null;
+  }
+
+  @Get('single')
+  async findOne(): Promise<any> {
+    return null;
+  }
+
+  @Post()
+  async save(@Body() form: UserRegistrationForm): Promise<any> {
+    console.debug('Validated : ', form);
+
     return null;
   }
 }
