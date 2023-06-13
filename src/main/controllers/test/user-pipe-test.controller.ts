2fdc48dc98df8f28e86885d611bd172b091b0650
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-13 13:58:03 +0800

    feat: include User Test Pipe Ctrller

--- src/main/controllers/test/user-pipe-test.controller.ts
+++ src/main/controllers/test/user-pipe-test.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../model/user.entity';
+import { Repository } from 'typeorm';
 
 @Controller('user/test/pipe')
 export class UserPipeTestController {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
   @Get()
   async findAll(): Promise<any> {
     return null;
--- src/main/core.module.ts
+++ src/main/core.module.ts
@@ -4,10 +4,15 @@ import { UserTestController } from './controllers/test/user-test.controller';
 import { UserDetailsController } from './controllers/user-details.controller';
 import { User } from './model/user.entity';
 import { UserDetails } from './model/user-details.entity';
+import { UserPipeTestController } from './controllers/test/user-pipe-test.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserDetails, User])],
   providers: [],
-  controllers: [UserTestController, UserDetailsController],
+  controllers: [
+    UserTestController,
+    UserDetailsController,
+    UserPipeTestController,
+  ],
 })
 export class CoreModule {}
