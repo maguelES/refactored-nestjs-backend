2b9df595d590abb49a9b6827fc217c296d13f8a5
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-14 10:19:17 +0800

    Update logger.middleware.ts

--- src/main/http/middleware/logger.middleware.ts
+++ src/main/http/middleware/logger.middleware.ts
@@ -4,8 +4,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): any {
-    console.debug('Request', req.url);
-    console.debug('Response', res.statusCode);
+    console.debug('Request ', req.url);
+    console.debug('Response ', res.statusCode);
 
     next();
   }
