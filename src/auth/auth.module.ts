d3b9ce68f828e848560902f4344ce23180c038db
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-15 15:11:58 +0800

    fix: import core module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
+import { CoreModule } from '../main/core.module';
 
 @Module({
+  imports: [CoreModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
