91c71e118d29a3942e8deeaac322b724b10bb637
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-15 13:51:23 +0800

    feat: save user in service

--- src/main/data/transfers/user-registration.form.ts
+++ src/main/data/transfers/user-registration.form.ts
@@ -1,5 +1,18 @@
+import { User } from '../../model/user.entity';
+
 export class UserRegistrationForm {
   first_name: string;
   last_name: string;
   email: string;
+
+  toEntity(): User {
+    return {
+      id: null,
+      firstName: this.first_name,
+      lastName: this.last_name,
+      createdAt: null,
+      updatedAt: null,
+      userDetails: null,
+    };
+  }
 }
---
+++ src/main/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../model/user.entity';
+import { UserRegistrationForm } from '../data/transfers/user-registration.form';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(form: UserRegistrationForm) {
+    return this.userRepository.save(form.toEntity());
+  }
+}
