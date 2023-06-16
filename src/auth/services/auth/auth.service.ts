3c5103e849378b519a5e311dc1437491a94606a5
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-16 15:12:01 +0800

    Update auth.service.ts

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from '../../../main/services/user.service';
 import { AuthRegistrationForm } from '../../data/transfers/auth-registration.form';
 import * as crypto from 'crypto';
+import * as bcrypt from 'bcrypt';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { UserRegistrationForm } from '../../../main/data/transfers/user-registration.form';
 
@@ -21,8 +22,10 @@ export class AuthService {
     const userForm = new UserRegistrationForm();
     userForm.firstName = form.firstName;
     userForm.lastName = form.lastName;
-    userForm.password = form.password;
     userForm.email = form.email;
+
+    const salt = await bcrypt.genSalt();
+    userForm.password = await bcrypt.hash(form.password, salt);
 
     console.debug(userForm);
     await this.userService.create(userForm);
