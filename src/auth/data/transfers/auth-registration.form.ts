5bd50c2f24b2c12145c6b55156113a7f239d3e59
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-16 14:39:56 +0800

    fix: rm max and expose

--- src/auth/data/transfers/auth-registration.form.ts
+++ src/auth/data/transfers/auth-registration.form.ts
@@ -2,23 +2,18 @@ import { Expose } from 'class-transformer';
 import { IsEmail, IsNotEmpty, Max, Min } from 'class-validator';
 
 export class AuthRegistrationForm {
-  @Max(100)
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @Min(4)
-  @Max(20)
   @IsNotEmpty()
   password: string;
 
-  @Max(100)
+  @Expose()
   @IsNotEmpty()
-  @Expose({ name: 'first_name' })
   firstName: string;
 
-  @Max(100)
+  @Expose()
   @IsNotEmpty()
-  @Expose({ name: 'last_name' })
   lastName: string;
 }
