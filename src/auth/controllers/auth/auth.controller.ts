2c2149d5e0629e36e051a872af196a02bd513e76
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-16 12:48:07 +0800

    fix: test register

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthRegistrationForm } from '../../data/transfers/auth-registration.form';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() form: AuthRegistrationForm): Promise<void> {
+    await this.authService.register(form);
+  }
+}
--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../../../main/services/user.service';
+import { AuthRegistrationForm } from '../../data/transfers/auth-registration.form';
+import * as crypto from 'crypto';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userservice: UserService) {}
+
+  async register(form: AuthRegistrationForm): Promise<void> {
+    const hash = crypto
+      .createHash('sha256')
+      .update(randomStringGenerator())
+      .digest('hex');
+
+    console.debug(hash);
+
+    await this.userservice.create({
+      ...form,
+      password: hash,
+    });
+
+    return null;
+  }
+}
