c3147ec858c7d8b883e5586b85cedf73cbf85a77
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-16 14:59:22 +0800

    feat: save user login without hash

--- src/main/services/user.service.ts
+++ src/main/services/user.service.ts
@@ -3,15 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../model/user.entity';
 import { UserRegistrationForm } from '../data/transfers/user-registration.form';
+import { UserLogin } from '../model/user-login.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(UserLogin)
+    private loginRepository: Repository<UserLogin>,
   ) {}
 
-  create(form: UserRegistrationForm): Promise<User> {
-    return this.userRepository.save(form.toEntity());
+  async create(form: UserRegistrationForm): Promise<User> {
+    const user = await this.userRepository.save(form.toEntity());
+
+    const login: UserLogin = {
+      user: user,
+      email: form.email,
+      password: form.password,
+      username: form.email,
+      id: null,
+    };
+
+    console.debug('login', login);
+    await this.loginRepository.save(login);
+
+    return user;
   }
 }
