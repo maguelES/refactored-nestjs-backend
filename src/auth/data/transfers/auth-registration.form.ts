3a133ded786f92ddd0828379bbe6e08193754acb
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-16 14:09:00 +0800

    feat: validate auth registration form

--- src/auth/data/transfers/auth-registration.form.ts
+++ src/auth/data/transfers/auth-registration.form.ts
@@ -1,13 +1,24 @@
 import { Expose } from 'class-transformer';
+import { IsEmail, IsNotEmpty, Max, Min } from 'class-validator';
 
 export class AuthRegistrationForm {
+  @Max(100)
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
+  @Min(4)
+  @Max(20)
+  @IsNotEmpty()
   password: string;
 
+  @Max(100)
+  @IsNotEmpty()
   @Expose({ name: 'first_name' })
   firstName: string;
 
+  @Max(100)
+  @IsNotEmpty()
   @Expose({ name: 'last_name' })
   lastName: string;
 }
