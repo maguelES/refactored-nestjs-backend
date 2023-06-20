7fcb278a9fc15330449b5a72cf6886100c41a57d
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-20 10:01:22 +0800

    fix: remove hashing

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -5,17 +5,13 @@ import * as crypto from 'crypto';
 import * as bcrypt from 'bcrypt';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { UserRegistrationForm } from '../../../main/data/transfers/user-registration.form';
+import { User } from '../../../main/model/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  async register(form: AuthRegistrationForm): Promise<void> {
-    const hash = crypto
-      .createHash('sha256')
-      .update(randomStringGenerator())
-      .digest('hex');
-
+  async register(form: AuthRegistrationForm): Promise<User> {
     const userForm = new UserRegistrationForm();
     userForm.firstName = form.firstName;
     userForm.lastName = form.lastName;
@@ -26,8 +22,7 @@ export class AuthService {
 
     const isMatch = await bcrypt.compare(form.password, userForm.password);
     console.debug(isMatch);
-    await this.userService.create(userForm);
 
-    return null;
+    return await this.userService.create(userForm);
   }
 }
