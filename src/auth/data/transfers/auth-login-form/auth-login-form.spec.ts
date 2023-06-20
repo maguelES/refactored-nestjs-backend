4ce2bdb2642c4269cf206fc43c7a6674e98a2eff
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-20 11:42:56 +0800

    feat: create auth login form

---
+++ src/auth/data/transfers/auth-login-form/auth-login-form.spec.ts
@@ -0,0 +1,7 @@
+import { AuthLoginForm } from './auth-login-form';
+
+describe('AuthLoginForm', () => {
+  it('should be defined', () => {
+    expect(new AuthLoginForm()).toBeDefined();
+  });
+});
