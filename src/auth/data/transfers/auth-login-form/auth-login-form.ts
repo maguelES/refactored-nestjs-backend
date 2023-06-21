d3aae38e9aec6b5bec5aee0a93594825f1f3174c
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-21 16:32:13 +0800

    Update auth-login-form.ts

--- src/auth/data/transfers/auth-login-form/auth-login-form.ts
+++ src/auth/data/transfers/auth-login-form/auth-login-form.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Length } from 'class-validator';
 
 export class AuthLoginForm {
+  @Length(4, 30)
   @IsNotEmpty()
   username: string;
 
   @IsNotEmpty()
+  @Length(4, 30)
   password: string;
 }
