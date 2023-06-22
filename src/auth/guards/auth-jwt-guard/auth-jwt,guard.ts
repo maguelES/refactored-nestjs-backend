fec619afad49ad91d81d1fe8029e27b0433edc77
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-22 11:57:01 +0800

    feat: create jwt auth guard

---
+++ src/auth/guards/auth-jwt-guard/auth-jwt,guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthJwtGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(req);
+
+    if (!token) throw new UnauthorizedException('No access token provided');
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get('auth.secret'),
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      req['user'] = payload;
+    } catch (exception) {
+      console.error(exception);
+
+      throw new UnauthorizedException('Cannot verify token');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
