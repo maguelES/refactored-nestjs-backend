1673408eee5db91e9d30d5ea5dcb99e9401efe31
Mohd Amsyar <nerokyt2@gmail.com>
2023-06-22 11:57:04 +0800

    Update auth-guard-test.controller.ts

--- src/auth/controllers/auth-guard-test/auth-guard-test.controller.ts
+++ src/auth/controllers/auth-guard-test/auth-guard-test.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../../main/model/user.entity';
+import { AuthJwtGuard } from '../../guards/auth-jwt-guard/auth-jwt,guard';
 
-@Controller('auth/test/guard')
+@Controller('auth/test/guards')
 export class AuthGuardTestController {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+  @UseGuards(AuthJwtGuard)
   @HttpCode(HttpStatus.OK)
   @Get()
   async findAll(): Promise<void> {
