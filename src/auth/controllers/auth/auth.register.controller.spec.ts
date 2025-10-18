d0c6b071bcf014a57a4521b6663fe1249cb0f27f
maguelES <nerokyt2@gmail.com>
2025-10-18 13:59:54 +0800

    test: auth register controller

--- src/auth/controllers/auth/auth.register.controller.spec.ts
+++ src/auth/controllers/auth/auth.register.controller.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthRegisterController } from './auth.register.controller';
+import { AuthModule } from '../../auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoreModule } from '../../../main/core.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AppModule } from '../../../app.module';
+import { AuthService } from '../../services/auth/auth.service';
 
 describe('AuthController', () => {
   let controller: AuthRegisterController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule, CoreModule, JwtModule, AppModule],
+      providers: [AuthService],
       controllers: [AuthRegisterController],
     }).compile();
 
