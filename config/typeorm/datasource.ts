485c552ec8fbad4834bbcb5f982ebf8220dc7cbb
maguelES <nerokyt2@gmail.com>
2025-10-18 09:50:16 +0800

    fix: database setup

--- config/typeorm/datasource.ts
+++ config/typeorm/datasource.ts
@@ -4,7 +4,7 @@ export const connectionSource = new DataSource({
   migrationsTableName: 'migrations',
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_SCHEMA,
